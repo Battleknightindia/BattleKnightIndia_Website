44f33fdfe7014cd87a4d5dfcbce2d33896669623
Battleknightindia <battleknightsindia@gmail.com>
2025-06-14 15:27:27 +0530

    add admin auth and force dynamic

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,36 +1,37 @@
 import AdminPanel from "./components/Homepage/AdminPanel";
 import { fetchteams } from "@/lib/data/stats_data";
-import  AdminLock from "./components/AdminLock";
+import AdminLock from "./components/AdminLock";
 import { createClient } from "@/utils/supabase/server";
 import { viewprofileSchema } from "@/schema/profileSchema";
 
+export const dynamic = 'force-dynamic';
 
 export default async function AdminPage() {
-  // const supabase = await createClient();
+  const supabase = await createClient();
 
-  // const {
-  //   data: { user },
-  //   error: userError,
-  // } = await supabase.auth.getUser();
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
 
-  // if (userError || !user) {
-  //   return <AdminLock />;
-  // }
+  if (userError || !user) {
+    return <AdminLock />;
+  }
 
-  // const { data: profile, error: profileError } = await supabase
-  //   .from("profiles")
-  //   .select("*")
-  //   .eq("user_id", user.id)
-  //   .single();
+  const { data: profile, error: profileError } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("user_id", user.id)
+    .single();
 
-  // if (profileError || !profile) {
-  //   return <AdminLock />;
-  // }
+  if (profileError || !profile) {
+    return <AdminLock />;
+  }
 
-  // const result = viewprofileSchema.safeParse(profile);
-  // if (!result.success || !result.data.is_admin) {
-  //   return <AdminLock />;
-  // }
+  const result = viewprofileSchema.safeParse(profile);
+  if (!result.success || !result.data.is_admin) {
+    return <AdminLock />;
+  }
 
   const teams = await fetchteams();
 
